0dd3454351541427de20f820d0e769ed63d669f4
Dannic12 <dannyx00new1@gmail.com>
2025-05-12 12:15:23 +0300

    add adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-$(function(){
+const btn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.header__right');
 
+btn.addEventListener('click', () => {
+  nav.classList.toggle('menu-open');
 });
